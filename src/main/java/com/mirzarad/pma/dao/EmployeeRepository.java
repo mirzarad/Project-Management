e8a0b4a020734843975dbfeb3e76980e038bf5a1
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-03-10 17:45:09 -0500

    Override findAll CRUD Repo method to return list of employees

--- src/main/java/com/mirzarad/pma/dao/EmployeeRepository.java
+++ src/main/java/com/mirzarad/pma/dao/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.mirzarad.pma.dao;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import com.mirzarad.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long>{
 
+	@Override
+	public List<Employee> findAll();
 }
