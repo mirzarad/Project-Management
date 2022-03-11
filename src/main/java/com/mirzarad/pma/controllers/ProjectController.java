f4eecd7c911d8fbb8490da33dcf5ea388486c2b0
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-03-11 18:49:52 -0500

    Update project controller to assign projects to employees table

--- src/main/java/com/mirzarad/pma/controllers/ProjectController.java
+++ src/main/java/com/mirzarad/pma/controllers/ProjectController.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mirzarad.pma.dao.EmployeeRepository;
 import com.mirzarad.pma.dao.ProjectRepository;
+import com.mirzarad.pma.entities.Employee;
 import com.mirzarad.pma.entities.Project;
 
 @Controller
@@ -18,6 +21,9 @@ public class ProjectController {
 	
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@Autowired
+	EmployeeRepository empRepo;
 	
 	@GetMapping
 	public String displayProjects(Model model) {
@@ -30,12 +36,24 @@ public class ProjectController {
 	public String displayProjectForm(Model model) {
 		Project aProject = new Project();
 		model.addAttribute("project", aProject);
+		
+		List<Employee> employees = empRepo.findAll();
+		model.addAttribute("allEmployees", employees);
 		return "projects/new-project";
 	}
 	
 	@PostMapping("/save")
-	public String createProject(Project project, Model model) {
+	public String createProject(Project project, @RequestParam List<Long> employees, Model model) {
+		
 		proRepo.save(project);
+		
+		Iterable<Employee> chosenEmployees = empRepo.findAllById(employees);
+		
+		for(Employee emp: chosenEmployees) {
+			emp.setProject(project);
+			empRepo.save(emp);
+		}
+		
 		return "redirect:/projects/new";
 	}
 	
