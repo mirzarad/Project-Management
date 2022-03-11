94770cf0d8d9ea3699375ff51f0c2b7e5f408c7c
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-03-11 01:49:36 -0500

    Update Project and Employee controllers to handle display respective list templates

--- src/main/java/com/mirzarad/pma/controllers/ProjectController.java
+++ src/main/java/com/mirzarad/pma/controllers/ProjectController.java
@@ -1,5 +1,7 @@
 package com.mirzarad.pma.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,13 @@ public class ProjectController {
 	
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@GetMapping
+	public String displayProjects(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projects", projects);
+		return "projects/list-projects";
+	}
 
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
