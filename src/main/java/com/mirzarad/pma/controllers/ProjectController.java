24d5ec7fd59f7bba26fa1e6df2a40eddc1dacea1
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-03-11 21:02:15 -0500

    Remove unecessary code for Proj/Emp save endpoint

--- src/main/java/com/mirzarad/pma/controllers/ProjectController.java
+++ src/main/java/com/mirzarad/pma/controllers/ProjectController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mirzarad.pma.dao.EmployeeRepository;
 import com.mirzarad.pma.dao.ProjectRepository;
@@ -43,16 +42,9 @@ public class ProjectController {
 	}
 	
 	@PostMapping("/save")
-	public String createProject(Project project, @RequestParam List<Long> employees, Model model) {
+	public String createProject(Project project, Model model) {
 		
 		proRepo.save(project);
-		
-		Iterable<Employee> chosenEmployees = empRepo.findAllById(employees);
-		
-		for(Employee emp: chosenEmployees) {
-			emp.setProject(project);
-			empRepo.save(emp);
-		}
 		
 		return "redirect:/projects/new";
 	}
