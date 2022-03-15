fc040233ddfa6805cb52e59b0564821acf6f7f44
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-03-15 18:33:54 -0400

    Add JS to decode thymeleaf object data and display project status summary on homepage

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,26 +1,51 @@
-const labels = [
-    'NOT STARTED',
-    'IN PROGRESS',
-    'COMPLETED',
-];
+var chartDataString = decodeHtml(chartData); // chartData comes from home.html
+var chartJsonArray = JSON.parse(chartDataString);
+
+var arrayLength = chartJsonArray.length;
+
+var numericData = [];
+var labelData = [];
+
+for(var i=0; i < arrayLength; i++){
+	numericData[i] = chartJsonArray[i].value;
+	labelData[i] = chartJsonArray[i].label;
+}
+
+const labels = labelData;
 
  const data = {
    labels: labels,
    datasets: [{
-     label: 'My First dataset',
-     backgroundColor: ['#3e95cd','#8e5ea2','#3cba9f'],
-     borderColor: 'rgb(255, 99, 132)',
-     data: [2, 10, 5],
+	   label: "Project Status",
+	   data: numericData,
+	   backgroundColor: [
+		   'rgb(77, 121, 255)',
+		   'rgb(255, 204, 0)',
+		   'rgb(224, 224, 235)'
+		   ],
+
+       borderWidth: 1
    }]
  };
 
  const config = {
    type: 'pie',
    data: data,
-   options: {}
+   options: {
+	   title: {
+		   display: true,
+		   text: "Project Statuses",
+	   }
+   }
  };
 
 var myPieChart = new Chart(
 		document.getElementById("myPieChart"),
 		config
 );
+
+function decodeHtml(html){
+	var text = document.createElement("textarea");
+	text.innerHTML = html;
+	return text.value;
+}
