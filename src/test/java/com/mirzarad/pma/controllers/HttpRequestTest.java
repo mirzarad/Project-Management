be5aa2fb59f8e54a6179729a135105c32d76e6ba
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-26 16:40:45 -0400

    Change configuration for dao test & add controller integration test package

---
+++ src/test/java/com/mirzarad/pma/controllers/HttpRequestTest.java
@@ -0,0 +1,5 @@
+package com.mirzarad.pma.controllers;
+
+public class HttpRequestTest {
+
+}
--- src/test/java/com/mirzarad/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/mirzarad/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.mirzarad.dao;
+package com.mirzarad.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -16,11 +17,10 @@ import com.mirzarad.pma.ProjectManagementApplication;
 import com.mirzarad.pma.dao.ProjectRepository;
 import com.mirzarad.pma.entities.Project;
 
-@ContextConfiguration(classes=ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql", "classpath:data.sql"}),
-	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts= "classpath:drop.sql")})
+//@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql", "classpath:data.sql"}),
+//	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts= "classpath:drop.sql")})
 public class ProjectRepositoryIntegrationTest {
 	
 	@Autowired
